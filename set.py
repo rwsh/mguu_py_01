caa1b77ac67c6aafaa6dcd4caab7c1299b70d516
Roman Shamin <roman@shamin.ru>
2024-11-26 16:56:15 +0300

    Add files via upload

---
+++ set.py
@@ -0,0 +1,8 @@
+A = {"Roman", "Elena", 120}
+B = {120, "Elena", "Alex"}
+
+C = A.intersection(B)
+
+print(A)
+print(B)
+print(C)
---
+++ tree.py
@@ -0,0 +1,5 @@
+s = 0
+for n in range(100):
+    s = s + (n + 1)*(n + 1)
+
+print(s)
